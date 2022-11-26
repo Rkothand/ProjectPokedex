1bcddc85ae4dae7cce48923458f572a30c528573
Rkothand <72056260+Rkothand@users.noreply.github.com>
2022-11-25 23:52:59 -0500

    working v1 of the pokeapi reactdex

---
+++ reactdex/src/App.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import './App.css';
+
+const App = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // useEffect(() => {
+  //   getPokemon();
+  // }, []);
+
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon();
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="enter pokemon name"
+          />
+        </label>
+      </form>
+      {pokemonData.map((data) => {
+        return (
+          <div className="container">
+            <img src={data.sprites["front_default"]} />
+              <div className="divTable">
+
+                <div className="divTableBody">
+                <div className="divTableRow">
+                  <div className="divTableCell">Type 1</div>
+                  <div className="divTableCell">{pokemonType}</div>
+                </div>
+
+                <div className="divTableRow">
+                  <div className="divTableCell">Average Height</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.height * 3.9)} "
+                  </div>
+                </div>
+
+                <div className="divTableRow">
+                  <div className="divTableCell">Average Weight</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.weight / 4.3)} lbs
+                  </div>
+
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Number Battled</div>
+                  <div className="divTableCell">{data.game_indices.length}</div>
+                </div>
+
+                
+              </div>
+
+          </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
