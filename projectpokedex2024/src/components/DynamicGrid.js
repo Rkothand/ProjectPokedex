81293abcf30339587e3d6d94031607cb56f99bf4
Rkothand <kothandaramanrohith@gmail.com>
2024-01-20 00:25:31 -0500

    cleaned up diff img displays

--- projectpokedex2024/src/components/DynamicGrid.js
+++ projectpokedex2024/src/components/DynamicGrid.js
@@ -32,21 +32,12 @@ const StyledItemImage = styled.img`
 `;
 
 const StyledItemButton = styled.button`
-  background-color: #4caf50;
+  background-color: ${({ isActive }) => (isActive ? '#4caf50' : '#ddd')};
   color: white;
   padding: 8px;
   border: none;
   cursor: pointer;
 `;
-
-// for (let i = 1; i <= count; i++) {
-//   PokedexDisplayData.push({
-//     id: i,
-//     name: `Item ${i}`,
-//     description: `Description ${i}`,
-//     imageUrl: `img/pokemon/${String(i).padStart(4, '0')}_000_mf_n.png`, // Assuming images are named like 0001_000_mf_n.png
-//   });
-// }
 
 const generatePokemonData = () => {
   const PokedexDisplayData = [];
@@ -54,17 +45,8 @@ const generatePokemonData = () => {
     if (PokemonData.hasOwnProperty(pokemonName)) {
       const pokemon = PokemonData[pokemonName];
 
-      // Log the Pokemon information
-      console.log(`Name: ${pokemon.name}`);
-      console.log(`Base ID: ${pokemon.base_id}`);
-      console.log(`Form ID: ${pokemon.form_id}`);
-      console.log(`Types: ${pokemon.pokemon_type.join(', ')}`);
-      console.log(`Egg Groups: ${pokemon.egg_group.join(', ')}`);
-      console.log(`Gender: ${pokemon.gender.join(', ')}`);
-      console.log(`Gender Ratio: ${pokemon.gender_ratio}`);
-      console.log(`Color: ${pokemon.color}`);
-      console.log(`Experience Group: ${pokemon.experience_group}`);
-      console.log(`Generation: ${pokemon.generation}`);
+      
+
 
       if (pokemon.form_id !== 0) {
         if ((pokemon.has_gigantamax || pokemon.is_mega)||pokemon.form_name) {
@@ -73,23 +55,13 @@ const generatePokemonData = () => {
         }
 
       }
-
-      if (pokemon.has_gigantamax) {
-        console.log("This Pokemon has a Gigantamax form.");
-      }
-
-      if (pokemon.is_mega) {
-        console.log("This Pokemon has a Mega form.");
-      }
-
-
-      //male and female no diff
-      if (pokemon.gender.length === 1 && pokemon.gender[0] === "mf") {
+      //no diff
+      if (pokemon.gender.length === 1) {
         PokedexDisplayData.push({
           id: pokemon.base_id,
           name: pokemon.name,
           description: pokemon.pokemon_type.join(', '),
-          imageUrl: `img/pokemon/${String(pokemon.base_id).padStart(4, '0')}_${String(pokemon.form_id).padStart(3, '0')}_mf_n.png`, // Assuming images are named like 0001_000_mf_n.png
+          imageUrl: `img/pokemon/${String(pokemon.base_id).padStart(4, '0')}_${String(pokemon.form_id).padStart(3, '0')}_${String(pokemon.gender[0])}_n.png`, // Assuming images are named like 0001_000_mf_n.png
         });
       }
 
@@ -103,24 +75,24 @@ const generatePokemonData = () => {
         });
       }
 
-      //male and female no diff
-      if (pokemon.gender.length === 1 && pokemon.gender[0] === "uk") {
-        PokedexDisplayData.push({
-          id: pokemon.base_id,
-          name: pokemon.name,
-          description: pokemon.pokemon_type.join(', '),
-          imageUrl: `img/pokemon/${String(pokemon.base_id).padStart(4, '0')}_${String(pokemon.form_id).padStart(3, '0')}_uk_n.png`, // Assuming images are named like 0001_000_mf_n.png
-        });
-      }
+      // //male and female no diff
+      // if (pokemon.gender.length === 1 && pokemon.gender[0] === "uk") {
+      //   PokedexDisplayData.push({
+      //     id: pokemon.base_id,
+      //     name: pokemon.name,
+      //     description: pokemon.pokemon_type.join(', '),
+      //     imageUrl: `img/pokemon/${String(pokemon.base_id).padStart(4, '0')}_${String(pokemon.form_id).padStart(3, '0')}_uk_n.png`, // Assuming images are named like 0001_000_mf_n.png
+      //   });
+      // }
 
-      if (pokemon.gender.length === 1 && pokemon.gender[0] === "fd") {
-        PokedexDisplayData.push({
-          id: pokemon.base_id,
-          name: pokemon.name,
-          description: pokemon.pokemon_type.join(', '),
-          imageUrl: `img/pokemon/${String(pokemon.base_id).padStart(4, '0')}_${String(pokemon.form_id).padStart(3, '0')}_fd_n.png`, // Assuming images are named like 0001_000_mf_n.png
-        });
-      }
+      // if (pokemon.gender.length === 1 && pokemon.gender[0] === "fd") {
+      //   PokedexDisplayData.push({
+      //     id: pokemon.base_id,
+      //     name: pokemon.name,
+      //     description: pokemon.pokemon_type.join(', '),
+      //     imageUrl: `img/pokemon/${String(pokemon.base_id).padStart(4, '0')}_${String(pokemon.form_id).padStart(3, '0')}_fd_n.png`, // Assuming images are named like 0001_000_mf_n.png
+      //   });
+      // }
 
 
 
@@ -135,26 +107,32 @@ const generatePokemonData = () => {
   return PokedexDisplayData;
 };
 
-
-
-
 const DynamicGrid = () => {
   const [gridData, setGridData] = useState([]);
 
   useEffect(() => {
-    // Generate mock data with 1000 images
-    const PokedexDisplayData = generatePokemonData();;
-    setGridData(PokedexDisplayData);
+    const pokedexDisplayData = generatePokemonData();
+    setGridData(pokedexDisplayData);
   }, []);
+
+  const handleButtonClick = (index) => {
+    setGridData((prevData) => {
+      const newData = [...prevData];
+      newData[index].isActive = !newData[index].isActive;
+      return newData;
+    });
+  };
 
   return (
     <StyledGrid>
-      {gridData.map((item) => (
+      {gridData.map((item, index) => (
         <StyledItem key={item.id}>
           <StyledItemImage src={item.imageUrl} alt={`Image for ${item.name}`} />
           <h3>{item.name}</h3>
           <p>{item.description}</p>
-          {/* <StyledItemButton>Click me</StyledItemButton> */}
+          <StyledItemButton onClick={() => handleButtonClick(index)} isActive={item.isActive}>
+            Click me
+          </StyledItemButton>
         </StyledItem>
       ))}
     </StyledGrid>
